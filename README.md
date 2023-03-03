a6798d24a96fe7267a317095e44c872b8a236c54
althierfson <althierfson.lima.109@ufrn.edu.br>
2023-03-02 21:07:00 -0300

    base projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Um simples jogo da velha
+
+Um simples jogo da velha feito em Flutter para você si divertir com seus amigos.
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:tic_tac_toe/home.dart';
+
+void main() {
+  runApp(const GetMaterialApp(
+    home: Home(),
+  ));
+}
